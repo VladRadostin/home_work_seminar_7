c524abf9916ba1b52129b00a1f92a16fd8d91c6e
vlad <vladradostinn@yandex.ru>
2023-07-06 04:58:14 +0300

    seminar_7

---
+++ Home_Work/Program.cs
@@ -0,0 +1,53 @@
+﻿
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    { 
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} "); 
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void doubleRandom(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            double double_1 = new Random().NextDouble();
+            int int_1 = new Random().Next(1, 100);
+            array[i,j] = Math.Round(double_1, 2) + int_1;
+        }
+    }
+}
+
+int m = 0;
+int n = 0;
+
+m = Prompt($"Введите число столбцов ");
+n = Prompt($"Введите число строк ");
+
+double[,] array = new double[m,n];
+
+PrintArray(array);
+doubleRandom(array);
+PrintArray(array);
---
+++ Home_Work_1/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    { 
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} "); 
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArrayRandom(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+double[,] array = new double[3,4];
+
+FillArrayRandom(array);
+PrintArray(array);
+
+
+int i = 0;
+int j = 0;
+
+double numbers = Prompt("Введите число ");
+
+Console.WriteLine();
+
+for (i = 0; i < array.GetLength(0); i++)
+{
+    for (j = 0; j < array.GetLength(1); j++)
+    {
+        if(array[i,j] == numbers)
+        {
+            Console.WriteLine($"Ваше число {numbers} находится на позиции {i},{j}");
+            return;
+        }
+    }
+}
+
+
+Console.WriteLine($"вашего числа нет");
